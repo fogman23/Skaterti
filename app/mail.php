b23e1c0dd0fd1f2a08baa0122b331d1803304cc7
Valera <fogman23@gmail.com>
2021-06-11 15:20:50 +0500

    завершил проект

---
+++ app/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "fogman23@gmail.com";
+$sitename = "Название сайта";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
